9315db1e38a34e5ece45328d7f013ed78c8995df
CsikiDaniel <64462491+CsikiDaniel@users.noreply.github.com>
2021-11-23 15:24:11 +0200

    Added more question and answer

--- app/src/main/java/com/quizapp/Constants.kt
+++ app/src/main/java/com/quizapp/Constants.kt
@@ -35,6 +35,69 @@ object Constants {
 
         questionList.add(question3)
 
+        val question4 = Question(1,
+            "What is the open keyword??",
+            "It can be derived from it.",
+            "You can't derail it.",
+            "Indicates an interface implementation.",
+            "Indicates an abstract class.",1)
+
+        questionList.add(question4)
+
+        val question5 = Question(1,
+            "What is the name of the construct block in kotlin?",
+            "override",
+            "super",
+            "init",
+            "class",3)
+
+        questionList.add(question5)
+
+        val question6 = Question(1,
+            "What does lateinite mean?",
+            "The type of variable val.",
+            "The expression only gets values later.",
+            "A variable var type.",
+            "Define a new type.",2)
+
+        questionList.add(question6)
+
+        val question7 = Question(1,
+            "What is the difference between val and var?",
+            "Val is immutable, var is mutable.",
+            "No difference.",
+            "Var is immutable, val is mutable.",
+            "Var is read-only.",1)
+
+        questionList.add(question7)
+
+        val question8 = Question(1,
+            "What is the difference between val and var?",
+            "Val is immutable, var is mutable.",
+            "No difference.",
+            "Var is immutable, val is mutable.",
+            "Var is read-only.",1)
+
+        questionList.add(question8)
+
+        val question9 = Question(1,
+            "What is the difference between val and var?",
+            "Val is immutable, var is mutable.",
+            "No difference.",
+            "Var is immutable, val is mutable.",
+            "Var is read-only.",1)
+
+        questionList.add(question9)
+
+        val question10 = Question(1,
+            "What is the difference between val and var?",
+            "Val is immutable, var is mutable.",
+            "No difference.",
+            "Var is immutable, val is mutable.",
+            "Var is read-only.",1)
+
+        questionList.add(question10)
+
         return questionList
 
     }
